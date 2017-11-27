7d794e3c97e241625fe762d04038ba4546ba4da6
Riccardo Bucco <riccardobucco@gmail.com>
2017-11-27 16:56:55 +0100

    Implement a basic Tree class

--- tree.py
+++ tree.py
@@ -28,3 +28,24 @@ class TreeNode:
 
     def hasChildren(self):
         return len(self.children)
+
+class Tree:
+
+    def __init__(self, root_node):
+        self.root = root_node
+    
+    def __iter__(self):
+        queue = []
+        all_nodes = []
+        queue.append(self.root)
+        while len(queue) is not 0:
+            current_node = queue.pop(0)
+            all_nodes.append(current_node)
+            queue.extend(current_node.getChildren())
+        return iter(all_nodes)
+
+    def getRoot(self):
+        return self.root
+
+    def setRoot(self, root_node):
+        self.root = root_node
