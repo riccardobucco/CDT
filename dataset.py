98e038ec78d513691f6884f5f3991499ebed1f83
Riccardo Bucco <riccardobucco@gmail.com>
2017-12-08 20:25:00 +0100

    Implement a basic DatasetInstance class

---
+++ dataset.py
@@ -0,0 +1,19 @@
+class DatasetInstance:
+
+    def __init(self, names, values, target):
+        self.attributes = {}
+        for index, name in enumerate(names):
+            self.attributes[name] = values[index]
+        self.target = target
+
+    def getAttribute(self, name):
+        return self.attributes[name]
+
+    def setAttribute(self, name, value):
+        self.attributes[name] = value
+
+    def getTarget(self):
+        return self.target
+
+    def setTarget(self, target):
+        self.target = target
