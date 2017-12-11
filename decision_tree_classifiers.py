7487ee2243db0546f4448ef7d9146dee393f558e
Riccardo Bucco <riccardobucco@gmail.com>
2017-12-11 17:10:01 +0100

    Refactor the decision_tree_classifiers module

--- decision_tree_classifiers.py
+++ decision_tree_classifiers.py
@@ -1,9 +1,40 @@
-from dataset import Dataset, DatasetInstance, copy_dataset
+from dataset import copy_dataset
 from decision_tree import DecisionNode, EndNode
 from utilities import informationGain
 
 
-def getBestAttribute(dataset):
+# PUBLIC FUNCTIONS
+def ID3(dataset):
+    """
+    Return a decision tree classifier, computed using the ID3 algorithm (DecisionNode or EndNode)
+    
+    Parameters:
+        - dataset: the dataset used to train the classifier
+    """
+    if len(dataset.getTargetValues()) is 1:
+        return DecisionTree(dataset.getTargetName(), dataset.getInstance(0).getTargetValue())
+    if len(dataset.getAttributesNames()) is 0:
+        return DecisionTree(dataset.getTargetName(), dataset.getMostCommonTarget())
+    best_attribute = _getBestAttribute(dataset)
+    decision_node = DecisionNode(best_attribute)
+    for attribute_value in dataset.getAttributeValues(best_attribute):
+        if dataset.countInstances(attribute = {"name": best_attribute, "value": attribute_value}) is 0:
+            child = EndNode(dataset.getMostCommonTarget())
+            decision_node.addChild(attribute_value, child)
+        else:
+            child = ID3(copy_dataset(dataset, attribute = {"name": best_attribute, "value": attribute_value}))
+            decision_node.addChild(attribute_value, child)
+    return decision_node
+
+# PRIVATE FUNCTIONS
+# These functions should not be used outside the module
+def _getBestAttribute(dataset):
+    """
+    Return the attribute of the dataset that best classifies examples of the dataset (String)
+    
+    Parameters:
+        - dataset: the dataset on which the computation has to be done (Dataset)
+    """
     attributes_names = dataset.getAttributesNames()
     max_information_gain = informationGain(dataset, attributes_names[0])
     best_attribute = attributes_names[0]
@@ -14,20 +45,3 @@ def getBestAttribute(dataset):
             max_information_gain = information_gain
             best_attribute = attribute_name
     return best_attribute
-
-def ID3(dataset):
-    # TODO: check if the dataset is empty
-    if len(dataset.getTargets()) is 1:
-        return DecisionTree(dataset.getTargetName(), dataset.getInstance(0).getTarget())
-    if len(dataset.getAttributesNames()) is 0:
-        return DecisionTree(dataset.getTargetName(), getMostCommonTarget(dataset))
-    best_attribute = getBestAttribute(dataset)
-    decision_tree = DecisionTree(splitting_attribute = best_attribute)
-    for attribute_value in dataset.getAttributes(best_attribute):
-        if dataset.countInstances(attribute = {"name": best_attribute, "value": attribute_value}) is 0:
-            child = DecisionTree(target = getMostCommonTarget(dataset))
-            decision_tree.addChild(attribute_value, child)
-        else:
-            child = ID3(copy_dataset(dataset, attribute = {"name": best_attribute, "value": attribute_value}))
-            decision_tree.addChild(attribute_value, child)
-    return decision_tree
