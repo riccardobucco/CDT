b361fa3192e0d2b9c676df82ab0a283e2fd52f07
Riccardo Bucco <riccardobucco@gmail.com>
2017-12-11 18:07:30 +0100

    Move entropy and informationGain to decision_tree_classifiers module

--- decision_tree_classifiers.py
+++ decision_tree_classifiers.py
@@ -1,3 +1,6 @@
+from __future__ import division
+from math import log
+
 from dataset import copy_dataset
 from decision_tree import DecisionNode, EndNode
 from utilities import informationGain
@@ -50,12 +53,39 @@ def _getBestAttribute(dataset):
         - dataset: the dataset on which the computation has to be done (Dataset)
     """
     attributes_names = dataset.getAttributesNames()
-    max_information_gain = informationGain(dataset, attributes_names[0])
+    max_information_gain = _informationGain(dataset, attributes_names[0])
     best_attribute = attributes_names[0]
     for index in range(1, len(attributes_names)):
         attribute_name = attributes_names[index]
-        information_gain = informationGain(dataset, attribute_name)
+        information_gain = _informationGain(dataset, attribute_name)
         if information_gain > max_information_gain:
             max_information_gain = information_gain
             best_attribute = attribute_name
     return best_attribute
+
+def _entropy(dataset):
+    """
+    Return the entropy that characterizes the given dataset (number)
+    
+    Parameters:
+        - dataset: the dataset on which the entropy has to be computed (Dataset)
+    """
+    proportions = [dataset.countInstances(target = target)/dataset.countInstances() for target in dataset.getTargetValues()]
+    return sum(-p*log(p, 2) for p in proportions)
+
+def _informationGain(dataset, attribute_name):
+    """
+    Return the measure of the difference in entropy from before to after the dataset is split on the
+    given attribute (in other words, how much uncertainty in the dataset was reduced after splitting
+    the dataset on the given attribute) (number)
+    
+    Parameters:
+        - dataset: the dataset on which the information gain has to be computed (Dataset)
+        - attribute_name: the name of the attribute with which the information gain has to be computed (String)
+    """
+    T = set()
+    attribute_values = dataset.getAttributesNames(attribute_name)
+    for attribute_value in attribute_values:
+        t = copy_dataset(dataset, attribute = {"name": attribute_name, "value": attribute_value})
+        T.add(t)
+    return _entropy(dataset) - sum(_entropy(t)*t.countInstances()/dataset.countInstances() for t in T)
