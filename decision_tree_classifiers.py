83bb8fb3e4eacc1bf3bd047949b24fc464a7d537
Riccardo Bucco <riccardobucco@gmail.com>
2017-12-12 18:48:55 +0100

    Add max_features to the random_forestr function

--- decision_tree_classifiers.py
+++ decision_tree_classifiers.py
@@ -1,19 +1,20 @@
 from __future__ import division
 from math import log
-from random import randint
+from random import randint, sample
 
 from dataset import copy_dataset, Dataset
 from decision_tree import DecisionNode, EndNode
 
 
 # PUBLIC FUNCTIONS
-def random_forest(dataset, n_of_trees):
+def random_forest(dataset, n_of_trees, max_features):
     """
     Return a list of decision trees, built using random samples of the dataset
 
     Parameters:
-        - dataset: the dataset used to train the classifier
-        - n_of_trees: the number of trees that have to be trained
+        - dataset: the dataset used to train the classifier (Dataset)
+        - n_of_trees: the number of trees that have to be trained (number)
+        - max_features: the number of features to consider when looking for the best split (number)
     """
     forest = []
     for i in range(0,n_of_trees):
@@ -23,29 +24,29 @@ def random_forest(dataset, n_of_trees):
             attributes_values = [random_instance.getAttributeValue(attribute_name) for attribute_name in dataset.getAttributesNames()]
             target_value = random_instance.getTargetValue()
             random_dataset.addInstance(attributes_values, target_value)
-        print(random_dataset)
-        forest.append(ID3(random_dataset))
+        forest.append(ID3(random_dataset, max_features))
     return forest
 
-def ID3(dataset):
+def ID3(dataset, max_features = None):
     """
     Return a decision tree classifier, computed using the ID3 algorithm (DecisionNode or EndNode)
     
     Parameters:
-        - dataset: the dataset used to train the classifier
+        - dataset: the dataset used to train the classifier (Dataset)
+        - max_features: the number of features to consider when looking for the best split (number)
     """
     if len(dataset.getTargetValues()) is 1:
         return EndNode(dataset.getInstance(0).getTargetValue())
     if len(dataset.getAttributesNames()) is 0:
         return EndNode(dataset.getMostCommonTarget())
-    best_attribute = _getBestAttribute(dataset)
+    best_attribute = _getBestAttribute(dataset, max_features)
     decision_node = DecisionNode(best_attribute)
     for attribute_value in dataset.getAttributeValues(best_attribute):
         if dataset.countInstances(attribute = {"name": best_attribute, "value": attribute_value}) is 0:
             child = EndNode(dataset.getMostCommonTarget())
             decision_node.addChild(attribute_value, child)
         else:
-            child = ID3(copy_dataset(dataset, attribute = {"name": best_attribute, "value": attribute_value}))
+            child = ID3(copy_dataset(dataset, attribute = {"name": best_attribute, "value": attribute_value}), max_features)
             decision_node.addChild(attribute_value, child)
     return decision_node
 
@@ -65,14 +66,17 @@ def classify(decision_tree, instance):
 
 # PRIVATE FUNCTIONS
 # These functions should not be used outside the module
-def _getBestAttribute(dataset):
+def _getBestAttribute(dataset, max_features = None):
     """
     Return the attribute of the dataset that best classifies examples of the dataset (String)
     
     Parameters:
         - dataset: the dataset on which the computation has to be done (Dataset)
+        - max_features: the number of features to consider when looking for the best split (number)
     """
     attributes_names = dataset.getAttributesNames()
+    if max_features is not None:
+        attributes_names = sample(attributes_names, min(max_features, len(attributes_names)))
     max_information_gain = _informationGain(dataset, attributes_names[0])
     best_attribute = attributes_names[0]
     for index in range(1, len(attributes_names)):
