1959abf590673f3b9ca74deef180847c70686480
Riccardo Bucco <riccardobucco@gmail.com>
2017-12-12 17:06:33 +0100

    Add a function that print a tree in the DOT language

--- utilities.py
+++ utilities.py
@@ -1,6 +1,7 @@
 import csv
 
 from dataset import Dataset, DatasetInstance
+from decision_tree import EndNode, DecisionNode
 
 
 def getDataset(filename, target_name):
@@ -20,6 +21,22 @@ def getDataset(filename, target_name):
 
 def exportGraphviz(filename, decision_tree):
     """
-    
+    Print on the output file info about a tree using the DOT language
+
+    Parameters:
+        - filename: name of the file where the content must be written
+        - decision_tree: the tree that has to be represented using the DOT language
     """
-    
+    # This function is private and should not be used outside the scope of the parent function
+    def _exportNode(node, output):
+        if type(node) is EndNode:
+            output.write("\t\"" + str(id(node)) + "\" [label=\"" + str(node.getTargetValue() + "\"]\n"))
+            return
+        output.write("\t\"" + str(id(node)) + "\" [label=\"\"]\n")
+        for decision_attribute_value in node.children.keys():
+            output.write("\t\"" + str(id(node)) + "\" -> \"" + str(id(node.getChild(decision_attribute_value))) + "\" [label=\"" + str(node.getDecisionAttribute()) + "=" + str(decision_attribute_value) + "\"]\n")
+            _exportNode(node.getChild(decision_attribute_value), output)
+    output = open(filename, 'w')
+    output.write("digraph G {\n")
+    _exportNode(decision_tree, output)
+    output.write("}")
