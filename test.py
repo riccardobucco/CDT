61267f4755ec17a5ead9291bf697d82d5d0168ac
Riccardo Bucco <riccardobucco@gmail.com>
2017-12-13 20:36:21 +0100

    Remove the test file

--- test.py
+++
@@ -1,6 +0,0 @@
-from utilities import get_dataset
-from decision_tree_classifiers import ID3, classify
-from dataset import DatasetInstance
-
-dataset = getDataset("pippo.csv", "d")
-decision_tree = ID3(dataset)
